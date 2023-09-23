bf95edac039aca67f90c61939b91ba256f0b2ad6
Bagoole ivan <ivanbagoole2001@gmail.com>
2023-09-23 19:16:49 +0300

    image change

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -6,14 +6,16 @@
 /* Step 2 - Variables */
 const fullName = 'Bagoole Ivan';
 
-let currentYear = Date.now;
+currentYear = Date.now;
 
-let profilePicture = 'E:\BYU class\Fall 23\cse121b\images\rsz_ivan_bagoole_-_pic.jpg';
+img.src = "E:\BYU class\Fall 23\cse121b\images\rsz_ivan_bagoole.jpg";
+
+var src = document.getElementById("image");
+
+src.appendChild(image);
 
 /* Step 3 - Element Variables */
-const nameElement = document.getElementById('Name');
-
-var elem = document. getElementById("button"); elem. innerHTML = "Enable"; 
+const nameElement = document.getElementsById('fullName');
 
 
 /*Above I'm changing the innerHTML of an element.*/
@@ -22,14 +24,11 @@ let foodElement = document.getElementById('food');
 
 let yearElement = document.querySelector('#year');
 
-let imageElement = document.getElementById('image');
-
 /* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
 
 document.getElementById('yearElement').textContent = currentYear;
 
-imageElement.setAttribute('src', profilePicture);
 
 /* Step 5 - Array */
 food[4] = {'Meal ': 'Rice ' , 'Source ' : 'Beef ' , 'Desert ' : 'Ice Cream'};
