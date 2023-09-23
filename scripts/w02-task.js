3637ecc85c7ebd4146217b2cf71c41c568b0acf1
Bagoole ivan <ivanbagoole2001@gmail.com>
2023-09-23 18:13:29 +0300

    java update

---
+++ scripts/w02-task.js
@@ -0,0 +1,40 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+/* Declaring variables for Name, Date, Time and picture */
+
+/* Step 2 - Variables */
+const fullName = 'Bagoole Ivan';
+
+let currentYear = Date.now;
+
+let profilePicture = 'E:\BYU class\Fall 23\cse121b\images\rsz_ivan_bagoole_-_pic.jpg';
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('Name');
+
+var elem = document. getElementById("button"); elem. firstChild. innerHTML = "Enable"; 
+
+
+/*Above I'm changing the innerHTML of an element.*/
+
+let foodElement = document.getElementById('food');
+
+let yearElement = document.querySelector('#year');
+
+let imageElement = document.getElementById('image');
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+
+document.getElementById('yearElement').textContent = currentYear;
+
+imageElement.setAttribute('src', profilePicture);
+
+/* Step 5 - Array */
+food[4] = {'Meal ': 'Rice ' , 'Source ' : 'Beef ' , 'Desert ' : 'Ice Cream'};
+
+
+
+
+
