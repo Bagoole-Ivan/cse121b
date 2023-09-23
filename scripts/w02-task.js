757470e7e02bebc2020e68b063ff3d52cac302a4
Bagoole ivan <ivanbagoole2001@gmail.com>
2023-09-23 20:31:46 +0300

    i'm lost

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -4,18 +4,16 @@
 /* Declaring variables for Name, Date, Time and picture */
 
 /* Step 2 - Variables */
-const fullName = 'Bagoole Ivan';
+let fullName = 'Bagoole Ivan';
 
 currentYear = Date.now;
 
-img.src = "E:\BYU class\Fall 23\cse121b\images\rsz_ivan_bagoole.jpg";
+var image = document.getElementById('image');
 
-var src = document.getElementById("image");
-
-src.appendChild(image);
+image.src = "E:\BYU class\Fall 23\cse121b\images\ivan_bagoole.png";
 
 /* Step 3 - Element Variables */
-const nameElement = document.getElementsById('fullName');
+let nameElement = document.getElementById('name');
 
 
 /*Above I'm changing the innerHTML of an element.*/
@@ -25,7 +23,7 @@ let foodElement = document.getElementById('food');
 let yearElement = document.querySelector('#year');
 
 /* Step 4 - Adding Content */
-nameElement.innerHTML = `<strong>${fullName}</strong>`;
+fullName = `<strong>${fullName}</strong>`;
 
 document.getElementById('yearElement').textContent = currentYear;
 
